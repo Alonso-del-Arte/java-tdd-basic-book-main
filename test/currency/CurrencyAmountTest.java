b6a9cad8e004547a82ed1851635f4d76ecc55611
Alonso del Arte <alonso.delarte@gmail.com>
2022-12-21 16:51:48 -0500

    Backtrack, kind of, but not fully

--- test/currency/CurrencyAmountTest.java
+++ test/currency/CurrencyAmountTest.java
@@ -271,7 +271,7 @@ public class CurrencyAmountTest {
 
     @Test
     public void testNegateDinars() {
-        int cents = RANDOM.nextInt(524288) - 262144;
+        int cents = -RANDOM.nextInt(524288) + 262144;
         CurrencyAmount amount = new CurrencyAmount(cents, DINARS);
         CurrencyAmount expected = new CurrencyAmount(-cents, DINARS);
         CurrencyAmount actual = amount.negate();
