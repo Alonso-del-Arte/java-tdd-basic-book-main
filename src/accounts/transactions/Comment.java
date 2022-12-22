d4c24e657ba4cb1812ee367fe6e238ed11777a73
Alonso del Arte <alonso.delarte@gmail.com>
2022-12-22 15:59:04 -0500

    Transaction subclasses

---
+++ src/accounts/transactions/Comment.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accounts.transactions;
+
+import currency.CurrencyAmount;
+
+import java.time.LocalDateTime;
+import java.util.Currency;
+
+/**
+ *
+ * @author Alonso del Arte
+ */
+public class Comment extends Transaction {
+    
+    public Comment(Currency currency, LocalDateTime time, String desc) {
+        super(new CurrencyAmount(0, currency), time, "Comment: " + desc);
+    }
+    
+}
---
+++ src/accounts/transactions/Withdrawal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accounts.transactions;
+
+import currency.CurrencyAmount;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Alonso del Arte
+ */
+public class Withdrawal extends Transaction {
+    
+    public Withdrawal(CurrencyAmount amt, LocalDateTime time) {
+        super(amt, time, "Withdrawal");
+    }
+    
+}
