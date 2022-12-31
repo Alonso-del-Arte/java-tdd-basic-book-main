67bb0bda3a01b387bfb52875bda52d803ef16952
Alonso del Arte <alonso.delarte@gmail.com>
2022-12-31 00:32:57 -0500

    A few stubs

--- src/entities/idnumbers/SocialSecurityNumber.java
+++ src/entities/idnumbers/SocialSecurityNumber.java
@@ -11,6 +11,26 @@ package entities.idnumbers;
  */
 public class SocialSecurityNumber extends TaxpayerIdentificationNumber {
     
+    // TODO: Write tests for this
+    public String toRedactedString() {
+        return "NOT IMPLEMENTED YET";
+    }
+    
+    // TODO: Write tests for this
+    public boolean matchesLast4(int partial) {
+        return false;
+    }
+    
+    // TODO: Write tests for this
+    public boolean matchesLast4(String numberText) {
+        return false;
+    }
+    
+    // TODO: Write tests for this
+    static boolean correctSSNDashPlacement(String s) {
+        return false;
+    }
+
     public SocialSecurityNumber(long number) {
         super(number);
     }
