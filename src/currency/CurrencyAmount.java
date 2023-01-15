11c9f0a8bbecc1bf5539487f81acb864f504c53e
Alonso del Arte <alonso.delarte@gmail.com>
2023-01-14 21:10:47 -0500

    Refactoring

--- src/currency/CurrencyAmount.java
+++ src/currency/CurrencyAmount.java
@@ -50,13 +50,7 @@ public class CurrencyAmount implements Comparable<CurrencyAmount> {
     @Override
     public int compareTo(CurrencyAmount other) {
         long diff = this.amountInCents - other.amountInCents;
-        if (diff > 0) {
-            return 1;
-        }
-        if (diff < 0) {
-            return -1;
-        }
-        return 0;
+        return Long.signum(diff);
     }
     
     @Override
