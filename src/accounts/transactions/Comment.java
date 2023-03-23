741201a8e1e99b70c331c055f21554d961de3bd0
Alonso del Arte <alonso.delarte@gmail.com>
2023-03-22 22:56:04 -0400

    Passing not equals different currency test

--- src/accounts/transactions/Comment.java
+++ src/accounts/transactions/Comment.java
@@ -38,7 +38,7 @@ public class Comment extends Transaction {
     
     @Override
     public boolean equals(Object obj) {
-        return obj instanceof Comment;
+        return this == obj;
     }
     
     @Override
