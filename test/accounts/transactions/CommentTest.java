f954b2a6d72fa81bbfa6ee95102035ee7546f683
Alonso del Arte <alonso.delarte@gmail.com>
2023-03-25 00:22:12 -0400

    Failing hashCode( ) test

--- test/accounts/transactions/CommentTest.java
+++ test/accounts/transactions/CommentTest.java
@@ -9,6 +9,8 @@ import currency.CurrencyAmount;
 
 import java.time.LocalDateTime;
 import java.util.Currency;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -21,6 +23,9 @@ public class CommentTest {
     
     private static final String DEFAULT_DESCRIPTION 
             = "For testing purposes only";
+    
+    private static final String PARTIAL_REWARDS_POINT_MESSAGE 
+            = " Rewards Points awarded";
     
     @Test
     public void testGetAmount() {
@@ -58,7 +63,7 @@ public class CommentTest {
     public void testToString() {
         System.out.println("toString");
         int points = TransactionTest.RANDOM.nextInt(1000);
-        String desc = points + " Rewards Points awarded";
+        String desc = points + PARTIAL_REWARDS_POINT_MESSAGE;
         Comment comment = new Comment(TransactionTest.DOLLARS, 
                 LocalDateTime.now(), desc);
         String expected = "Comment: " + desc;
@@ -126,6 +131,26 @@ public class CommentTest {
         Comment commentB = new Comment(TransactionTest.DOLLARS, curr, 
                 DEFAULT_DESCRIPTION.toUpperCase());
         assertNotEquals(commentA, commentB);
+    }
+    
+    @Test
+    public void testHashCode() {
+        System.out.println("hashCode");
+        int capacity = TransactionTest.RANDOM.nextInt(32) + 8;
+        Set<Comment> comments = new HashSet<>(capacity);
+        Set<Integer> hashes = new HashSet<>(capacity);
+        int offset = 100;
+        int end = offset + capacity;
+        for (int i = offset; i < end; i++) {
+            String text = i + PARTIAL_REWARDS_POINT_MESSAGE;
+            Comment comment = new Comment(TransactionTest.DOLLARS, 
+                    LocalDateTime.now(), text);
+            comments.add(comment);
+            hashes.add(comment.hashCode());
+        }
+        int expected = comments.size();
+        int actual = hashes.size();
+        assertEquals(expected, actual);
     }
     
 }
