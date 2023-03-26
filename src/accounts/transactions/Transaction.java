7bba6853c48fdbb9f9eae54649c4caddfd4cbad4
Alonso del Arte <alonso.delarte@gmail.com>
2023-03-25 20:03:45 -0400

    Refactoring

--- src/accounts/transactions/Transaction.java
+++ src/accounts/transactions/Transaction.java
@@ -35,17 +35,6 @@ public class Transaction {
     }
     
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        return this.getClass().equals(obj.getClass());
-    }
-    
-    @Override
     public int hashCode() {
         return 0;
     }
