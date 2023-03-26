8d19cb49c2bcc751215a988482ecb589d3a71783
Alonso del Arte <alonso.delarte@gmail.com>
2023-03-26 02:29:51 -0400

    Refactoring

--- src/accounts/transactions/Deposit.java
+++ src/accounts/transactions/Deposit.java
@@ -36,30 +36,6 @@ public class Deposit extends Transaction {
                 + this.dateTime.toString();
     }
     
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (!this.getClass().equals(obj.getClass())) {
-            return false;
-        }
-        Deposit other = (Deposit) obj;
-        if (!this.amount.equals(other.amount)) {
-            return false;
-        }
-        return this.dateTime.equals(other.dateTime);
-    }
-    
-    @Override
-    public int hashCode() {
-        int hash = this.amount.hashCode() << 16;
-        return hash + this.dateTime.hashCode();
-    }
-    
     public Deposit(CurrencyAmount amt, LocalDateTime time) {
         super(amt, time, "Deposit");
         if (amt.getAmountInCents() < 1L) {
--- src/accounts/transactions/Withdrawal.java
+++ src/accounts/transactions/Withdrawal.java
@@ -36,30 +36,6 @@ public class Withdrawal extends Transaction {
                 + this.dateTime.toString();
     }
     
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (!this.getClass().equals(obj.getClass())) {
-            return false;
-        }
-        Withdrawal other = (Withdrawal) obj;
-        if (!this.amount.equals(other.amount)) {
-            return false;
-        }
-        return this.dateTime.equals(other.dateTime);
-    }
-    
-    @Override
-    public int hashCode() {
-        int hash = this.amount.negate().hashCode() << 16;
-        return hash + this.dateTime.hashCode();
-    }
-    
     public Withdrawal(CurrencyAmount amt, LocalDateTime time) {
         super(amt, time, "Withdrawal");
         if (amt.getAmountInCents() > -1L) {
