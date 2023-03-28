cf3201dd8d51acc2a19548b6041fbe3810007d5b
Alonso del Arte <alonso.delarte@gmail.com>
2023-03-27 22:59:08 -0400

    Passing compare( ) test

--- src/accounts/transactions/comparators/AmountComparator.java
+++ src/accounts/transactions/comparators/AmountComparator.java
@@ -6,31 +6,36 @@
 package accounts.transactions.comparators;
 
 import accounts.transactions.Transaction;
-import currency.CurrencyAmount;
-import currency.CurrencyConversionNeededException;
 
 import java.util.Comparator;
-import java.util.Currency;
 
 /**
- *
+ * Comparator for two transactions according to their amounts. Lists sorted with 
+ * this comparator consist first of withdrawals, if any, then comments, if any, 
+ * and lastly deposits if any.
  * @author Alonso del Arte
  */
 public class AmountComparator implements Comparator<Transaction> {
 
+    /**
+     * Compares two transactions according to their amounts. The two 
+     * transactions should be drawn in the same currency.
+     * @param trxA The first transaction. For example, a deposit of $200.00.
+     * @param trxB The second transaction. For example, a withdrawal of $10.00.
+     * @return 0 if the two transactions are equal in amount, a negative number 
+     * (most likely &minus;1) if <code>trxA</code> is for less than 
+     * <code>trxB</code>, or a positive number (most likely 1) if 
+     * <code>trxA</code> is for more than <code>trxB</code>. In the example 
+     * given above, this function would most likely return 1 but it may return 
+     * some other positive integer.
+     * @throws currency.CurrencyConversionNeededException If <code>trxA</code> 
+     * and <code>trxB</code> are of different currencies, e.g., if 
+     * <code>trxA</code> is a deposit of euros and <code>trxB</code> is a 
+     * deposit of yen.
+     */
     @Override
     public int compare(Transaction trxA, Transaction trxB) {
-        CurrencyAmount amountA = trxA.getAmount();
-        CurrencyAmount amountB = trxB.getAmount();
-        Currency currencyA = amountA.getCurrency();
-        Currency currencyB = amountB.getCurrency();
-        if (currencyA != currencyB) {
-            String excMsg = "Currency conversion needed to compare " 
-                    + trxA.toString() + " to " + trxB.toString();
-            throw new CurrencyConversionNeededException(excMsg, amountA, 
-                    amountB);
-        }
-        return 0;
+        return trxA.getAmount().compareTo(trxB.getAmount());
     }
     
 }
