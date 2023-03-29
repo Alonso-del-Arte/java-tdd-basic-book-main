bf3dba2ec1688ee68e2e07ddfeb47a6f8d0a7766
Alonso del Arte <alonso.delarte@gmail.com>
2023-03-28 22:56:58 -0400

    Passing compare( ) test

--- src/accounts/transactions/comparators/DateComparator.java
+++ src/accounts/transactions/comparators/DateComparator.java
@@ -11,15 +11,30 @@ import java.time.LocalDateTime;
 import java.util.Comparator;
 
 /**
- *
+ * Comparator for two transactions according to their timestamps. Lists sorted 
+ * with this comparator have the most recent transaction last and the least 
+ * recent transaction first.
  * @author Alonso del Arte
  */
 public class DateComparator implements Comparator<Transaction> {
     
-    // TODO: Write tests for this
+    /**
+     * Compares two transactions according to their timestamps. The two 
+     * transactions need not be drawn in the same currency.
+     * @param trxA A transaction. For example, the very first deposit to the 
+     * account.
+     * @param trxB A transaction. For example, a direct deposit scheduled for 
+     * next Friday.
+     * @return 0 if the two transactions are on the same date and time, a 
+     * negative number (most likely &minus;1) if <code>trxA</code> took place 
+     * before <code>trxB</code>, or a positive number (most likely 1) if 
+     * <code>trxA</code> took place after <code>trxB</code>. In the example 
+     * given above, this function would most likely return &minus;1 but it may 
+     * return some other negative integer.
+     */
     @Override
     public int compare(Transaction trxA, Transaction trxB) {
-        return 0;
+        return trxA.getTime().compareTo(trxB.getTime());
     }
     
 }
