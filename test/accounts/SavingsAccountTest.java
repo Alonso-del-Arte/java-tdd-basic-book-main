6bc7e528a2c9af0136761e73fc338e2c89571894
Alonso del Arte <alonso.delarte@gmail.com>
2023-04-03 23:00:09 -0400

    Failing process( ) test

--- test/accounts/SavingsAccountTest.java
+++ test/accounts/SavingsAccountTest.java
@@ -7,11 +7,14 @@ package accounts;
 
 import accounts.transactions.Deposit;
 import accounts.transactions.Transaction;
+import static accounts.transactions.TransactionTest.makeTransaction;
 import accounts.transactions.Withdrawal;
 import currency.CurrencyAmount;
 import static entities.ExampleEntities.EXAMPLE_CUSTOMER;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -31,6 +34,23 @@ public class SavingsAccountTest {
         SavingsAccount account 
                 = new SavingsAccount(EXAMPLE_CUSTOMER, deposit);
         CurrencyAmount actual = account.balance;
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void testProcess() {
+        System.out.println("process");
+        Deposit initialDeposit = new Deposit(AccountTest
+                .DEFAULT_INITIAL_DEPOSIT_AMOUNT, 
+                LocalDateTime.now());
+        SavingsAccount account = new SavingsAccount(EXAMPLE_CUSTOMER, 
+                        initialDeposit);
+        Transaction secondTrx = makeTransaction();
+        account.process(secondTrx);
+        List<Transaction> expected = new ArrayList<>();
+        expected.add(initialDeposit);
+        expected.add(secondTrx);
+        List<Transaction> actual = account.getHistory();
         assertEquals(expected, actual);
     }
     
