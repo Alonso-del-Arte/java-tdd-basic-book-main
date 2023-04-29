96c55180ede3d17a639bd1719dc395a8fca9aabd
Alonso del Arte <alonso.delarte@gmail.com>
2023-04-28 21:07:14 -0400

    Refactoring; added some Javadoc

--- src/accounts/CheckingAccount.java
+++ src/accounts/CheckingAccount.java
@@ -15,24 +15,48 @@ import entities.Entity;
 import java.util.Currency;
 
 /**
- *
+ * Represents a checking account. A savings account can be connected to a 
+ * checking account to provide overdraft transfers.
  * @author Alonso del Arte
  */
 public class CheckingAccount extends Account {
     
-    private boolean associateFlag = false;
-    
     private SavingsAccount associatedSavings = null;
     
+    /**
+     * Tells whether or not this checking account has an associated savings 
+     * account. If it does, the associated savings account might be able to 
+     * cover deficits incurred in checking account transactions. A savings 
+     * account is associated by calling {@link 
+     * #associate(accounts.SavingsAccount) associate()}.
+     * @return True if this checking account has an associated savings account, 
+     * false otherwise.
+     */
     public boolean hasAssociatedSavingsAccount() {
         return this.associatedSavings != null;
     }
     
+    /**
+     * Associates this checking account to a savings account, or severs a 
+     * previously made association. Whether or not this checking account has an 
+     * associated savings account can be queried by calling {@link 
+     * #hasAssociatedSavingsAccount()}.
+     * @param savings The savings account to associate this checking account 
+     * with. May be null in order to disassociate this checking account from any 
+     * savings account.
+     */
     public void associate(SavingsAccount savings) {
-        this.associateFlag = true;
         this.associatedSavings = savings;
     }
     
+    /**
+     * Getter for the associated savings account. A savings account is 
+     * associated by calling {@link #associate(accounts.SavingsAccount) 
+     * associate()}. Whether or not there's an associated savings account can be 
+     * queried by calling {@link #hasAssociatedSavingsAccount()}.
+     * @return The associated savings account. Null if there's no currently 
+     * associated savings account.
+     */
     public SavingsAccount getAssociatedSavings() {
         return this.associatedSavings;
     }
